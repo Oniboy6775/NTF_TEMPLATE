487c9b45ae3417600c98535f03e0a78838fda47c
Oniboy6775 <51511136+Oniboy6775@users.noreply.github.com>
2021-11-06 10:11:59 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useWeb3React } from "@web3-react/core";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import "./App.css";
+import Loader from "./components/Loader/Loader";
+
+const App = () => {
+  const { active } = useWeb3React();
+
+  let mainContent = (
+    <>
+      <Route
+        exact
+        path="/"
+        component={React.lazy(() => import("./views/Login/Login"))}
+      />
+      {localStorage.getItem("userData") === null && <Redirect to="/" />}
+    </>
+  );
+
+  if (active) {
+    localStorage.setItem("shouldEagerConnect", true);
+
+    mainContent = (
+      <>
+        <Route
+          path="/"
+          component={React.lazy(() =>
+            import("./views/MainContainer/MainContainer")
+          )}
+        />
+      </>
+    );
+  }
+
+  return (
+    <React.Suspense fallback={<Loader />}>
+      <BrowserRouter>
+        <Switch>{mainContent}</Switch>
+      </BrowserRouter>
+    </React.Suspense>
+  );
+};
+
+export default App;
